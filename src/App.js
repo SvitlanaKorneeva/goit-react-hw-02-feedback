2fdf7b5258815274f3a4d4580f60cb56c933c79e
Корнеева Светлана <s.korneeva@brsm.com>
2021-02-26 17:19:23 +0200

    Statistics added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Profile from "./components/Profile/Profile";
-// import Statistics from "./components/Statistics/Statistics";
+import Statistics from "./components/Statistics/Statistics";
 // import FriendList from "./components/FriendList/FriendList";
 // import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import profileUser from "./db/user.json";
-// import statisticalData from "./db/statistical-data.json";
+import statisticalData from "./db/statistical-data.json";
 // import friends from "./db/friends.json";
 // import transactions from "./db/transactions.json";
-
+console.log(statisticalData);
 const { name, tag, location, avatar, stats } = profileUser;
 
 function App() {
@@ -21,9 +21,10 @@ function App() {
         avatar={avatar}
         stats={stats}
       />
-      {/* <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} /> */}
+      <Statistics title="Upload stats" stats={statisticalData} />
+      {/* <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />  */}
+      
       <h1>hello my lovely Fe-25</h1>
     </>
   );
