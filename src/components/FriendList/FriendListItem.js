225de555651b24f4bdff1e2f9ef2cc0fab530d04
Корнеева Светлана <s.korneeva@brsm.com>
2021-03-02 17:59:25 +0200

    styles added to FriendList

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import s from './FriendListItem.module.css'
+import s from './FriendListItem.module.css';
 
-const FriendListItem = ({avatar, name, isOnline})  => {
-    return (
-        <li className ={s.item}>
-            <span className={s.status}></span>
-            <img className={s.avatar} scr ={avatar} alt = {name} width="48" />
-            <p className={s.name}>{name}</p>
-        </li>
-    );
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={s.item}>
+      <span className={s.status} ></span>
+      <img className={s.avatar } src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
 };
 
 export default FriendListItem;
