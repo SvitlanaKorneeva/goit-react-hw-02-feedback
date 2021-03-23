624113a3ac8979513f3050a71ad8b061a699864c
Svitlana Korneeva <kso16101@gmail.com>
2021-03-23 17:57:03 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
+import React, { Component } from "react";
 import "./App.css";
 import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
 
 
+class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  };
+  
+  // handleGood = () => {
+  //   this.setState(prevState => ({
+  //     good: prevState.good +1,
 
-function App() {
-  return (
-    <>
+  //   }))
+  // };
+  // handleNeutral = () => {
+  //   this.setState(prevState => ({
+  //     neutral: prevState.neutral +1,
+
+  //   }))
+  // };
+  
+  // handleBad = () => {
+  //   this.setState(prevState => ({
+  //     bad: prevState.bad +1,
+
+  //   }))
+  // };
+  onLeaveFeedback = (e) => {
+    const option = e.target.option;
+    this.setState((prevState) => ({
+      [option]: prevState[option] + 1
+    }));
+  };
+  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+		return good + neutral + bad;
+  };
+  
+countPositiveFeedbackPercentage = () => {
+		const result = this.countTotalFeedback();
+		const { good } = this.state;
+		const percentage = (good * 100) / result;
+		return Math.round(percentage);
+};
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+  }
+  return ( 
     
-      <Statistics />
+      < Section title="Please leave feedback">
+      <FeedbackOptions options = {['good', 'neutral', 'bad']} onLeaveFeedback ={this.onLeaveFeedback}/>
+      </ >
+      <Statistics
+      
+      good={good}
+      neutral={neutral} 
+      bad={bad} 
+      total={total} 
+      positivePercentage={positivePercentage} />
     
-    </>
   );
 }
 
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,54 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import s from "./Statistics.module.css";
 
-class Statistics extends Component {
-  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-  };
-  
-  handleGood = () => {
-    this.setState(prevState => ({
-      good: prevState.good +1,
-
-    }))
-  };
-  handleNeutral = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral +1,
-
-    }))
-  };
-  
-  handleBad = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad +1,
-
-    }))
-  };
-
-  render() {
-  console.log(this.state)
+const Statistics = ( {good, neutral, bad, total, positivePercentage}) => {
   return (
-    
   <div>
-      <h2>Please leave feedback</h2>
+      {/* <h2>Please leave feedback</h2>
       <div className={s.buttons}>
         <button type="button" onClick={this.handleGood }>Good</button>
         <button type="button" onClick={this.handleNeutral}>Neutral</button>
         <button type="button" onClick={this.handleBad }>Bad</button>
       </div>
-      <h2>Statistics</h2>
+      <h2>Statistics</h2> */}
       <div>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}  </p>
-        <p>Bad: {this.state.bad} </p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}  </p>
+        <p>Bad: {bad} </p>
+        <p>Total: {total} </p>
+        <p>Positive feedback: {positivePercentage} %</p>
       </div>
     </div>
   );
   
-}
 }
   
 export default Statistics;
